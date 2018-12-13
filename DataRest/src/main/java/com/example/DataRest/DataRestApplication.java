d5a774c8e48e06eeba792e13d19020f6c50489ea
wglsky <40151406+wglsky@users.noreply.github.com>
2018-12-13 17:35:21 +0100

    upload all

---
+++ DataRest/src/main/java/com/example/DataRest/DataRestApplication.java
@@ -0,0 +1,14 @@
+package com.example.DataRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRestApplication.class, args);
+	}
+
+}
+
